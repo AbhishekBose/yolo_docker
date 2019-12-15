1377a73b38a95cfdde37ddc215a6f90ecbd407b0
AbhishekBose <abose550@gmail.com>
2019-12-15 21:00:19 +0530

    latest commit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04
+MAINTAINER abhishek bose 
+
+RUN apt-get update -y && apt-get install -y python3-pip python3-dev libsm6 libxext6 libxrender-dev
+
+
+RUN \
+	apt-get install -y \
+	wget \
+	unzip \
+	ffmpeg \ 
+	git
+
+RUN pip3 intall numpy
+RUN pip3 install opencv-python
+RUN pip3 install requests
+RUN pip3 install numba
+RUN pip3 install imutils
+
+WORKDIR home/
+
+RUN git clone https://github.com/pjreddie/darknet
+WORKDIR darknet/
+
+RUN sed -i 's/GPU=.*/GPU=1/' Makefile 
+RUN sed -i 's/CUDNN=.*/CUDNN=1/' Makefile && \
+	make
+
+
+RUN wget https://pjreddie.com/media/files/yolov3.weights -P weights/
+
+WORKDIR /home
+
+
---
+++ detect_object.py
@@ -0,0 +1,194 @@
+#%%
+from ctypes import *
+import random
+import argparse
+import os
+import traceback
+import cv2
+import functools
+import numpy as np
+import time
+import sys
+import imutils
+#%%
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def sample(probs):
+    s = sum(probs)
+    probs = [a/s for a in probs]
+    r = random.uniform(0, 1)
+    for i in range(len(probs)):
+        r = r - probs[i]
+        if r <= 0:
+            return i
+    return len(probs)-1
+
+def c_array(ctype, values):
+    arr = (ctype*len(values))()
+    arr[:] = values
+    return arr
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int)]
+
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+
+
+libdarknet_path = os.path.join("/home/darknet/libdarknet.so")
+lib = CDLL(libdarknet_path, RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+predict = lib.network_predict
+predict.argtypes = [c_void_p, POINTER(c_float)]
+predict.restype = POINTER(c_float)
+
+set_gpu = lib.cuda_set_device
+set_gpu.argtypes = [c_int]
+
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+get_network_boxes = lib.get_network_boxes
+get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
+get_network_boxes.restype = POINTER(DETECTION)
+
+make_network_boxes = lib.make_network_boxes
+make_network_boxes.argtypes = [c_void_p]
+make_network_boxes.restype = POINTER(DETECTION)
+
+free_detections = lib.free_detections
+free_detections.argtypes = [POINTER(DETECTION), c_int]
+
+free_ptrs = lib.free_ptrs
+free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+network_predict = lib.network_predict
+network_predict.argtypes = [c_void_p, POINTER(c_float)]
+
+reset_rnn = lib.reset_rnn
+reset_rnn.argtypes = [c_void_p]
+
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+do_nms_obj = lib.do_nms_obj
+do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+do_nms_sort = lib.do_nms_sort
+do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+free_image = lib.free_image
+free_image.argtypes = [IMAGE]
+
+letterbox_image = lib.letterbox_image
+letterbox_image.argtypes = [IMAGE, c_int, c_int]
+letterbox_image.restype = IMAGE
+
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+load_image = lib.load_image_color
+load_image.argtypes = [c_char_p, c_int, c_int]
+load_image.restype = IMAGE
+
+rgbgr_image = lib.rgbgr_image
+rgbgr_image.argtypes = [IMAGE]
+
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+def classify(net, meta, im):
+    out = predict_image(net, im)
+    res = []
+    for i in range(meta.classes):
+        res.append((meta.names[i], out[i]))
+    res = sorted(res, key=lambda x: -x[1])
+    return res
+
+def array_to_image(arr):
+    # need to return old values to avoid python freeing memory
+    arr = arr.transpose(2,0,1)
+    c, h, w = arr.shape[0:3]
+    arr = np.ascontiguousarray(arr.flat, dtype=np.float32) / 255.0
+    data = arr.ctypes.data_as(POINTER(c_float))
+    im = IMAGE(w,h,c,data)
+    return im, arr
+
+def netdetect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    im, image = array_to_image(image)
+    rgbgr_image(im)
+    num = c_int(0)
+    pnum = pointer(num)
+    predict_image(net, im)
+    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
+    num = pnum[0]
+    if (nms): do_nms_obj(dets, num, meta.classes, nms);
+
+    res = []
+    for j in range(num):
+        a = dets[j].prob[0:meta.classes]
+        if any(a):
+            ai = np.array(a).nonzero()[0]
+            for i in ai:
+                b = dets[j].bbox
+                res.append((meta.names[i], dets[j].prob[i], (max(b.x,0), max(b.y,0), max(b.w,0), max(b.h,0))))
+    res = sorted(res, key=lambda x: -x[1])
+    if isinstance(image,bytes): free_image(im)
+    free_detections(dets, num)
+    return res
+
+#%%
+def convert_yolo_normal(x,y,w,h,s1,s2):
+    b0=(2*x-w)/2
+    b1=w+b0
+    b2=(2*y-h)/2
+    b3=h+b2
+    return (int(b0),int(b1),int(b2),int(b3))
+#%%
+if __name__ == "__main__":
+    config_file = 'object_detector.config'
+    with open('config.json') as f:
+        data = json.load(f)
+    weights_file = data['weights_file']
+    cfg_file = data['cfg_file']
+    obj_data =  data['obj_file']
+
+    image_name = sys.argv[1]
+    img = cv2.imread(image_name)
+
+    netdet = load_net(cfg_file,weights_file,0)
+    metadet = load_meta(obj_data)   
+
+    obj_res = netdetect(netlp,metalp, veh,0.7)
+    for obj_res:
+        print('All detected objects are:: ')
+        for i in range(len(obj_res)):
+            print(obj_res[i][0])
